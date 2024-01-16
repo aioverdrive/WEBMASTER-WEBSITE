2dc138c8117def665490408105748f8715c253f9
aymenawol <142862147+aymenawol@users.noreply.github.com>
2024-01-15 17:10:04 -0800

    Create z-practice.js
    some sample js code I might use

---
+++ z-practice.js
@@ -0,0 +1,11 @@
+// GSAP Animation with ScrollTrigger
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.utils.toArray('.section').forEach(section => {
+        ScrollTrigger.create({
+            trigger: section,
+            start: 'top top',
+            pin: true,
+            pinSpacing: false
+        });
+    });
